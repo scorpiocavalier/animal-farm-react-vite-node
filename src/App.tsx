5aaf26ce3184df77f9dd1d44ee2eec7c7363d59e
Arturo Chen <art.can.fly@gmail.com>
2022-10-18 15:40:00 -0400

    get animal results from BE

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-function App() {
-  return (
-    <main>
-      
-    </main>
-  )
+import { useState } from 'react';
+
+export interface IAnimal {
+  id: number,
+  type: string,
+  name: string,
+  age: number
 }
 
-export default App
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const search = async (q: string) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    );
+
+    const data = await response.json();
+
+    setAnimals(data);
+  }
+
+  return (
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input type='text' placeholder='Search' onChange={(e) => search(e.target.value)} />
+
+      <ul>
+        {!animals.length && 'No animals found'}
+
+        {animals.map((animal: IAnimal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Animal({ type, name, age }: IAnimal): JSX.Element {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+export default App;
